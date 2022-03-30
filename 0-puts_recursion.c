376825f5435ced62144cc6b77864c3bf45669637
root <tsholofelomakhoana@gmail.com>
2022-03-30 15:10:59 -0700

    0-puts_recursion.c

--- 0-puts_recursion.c
+++ 0-puts_recursion.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
-  * _puts_recursion - Print a string followed by a new line
-  * @s: the string to print
-  *
-  * Return: Nothing.
-  */
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: string
+ *
+ * Return: 1 id successful
+ * error, return -1
+ */
+
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
-
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
+if (*s == 0)
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(s + 1);
 }
